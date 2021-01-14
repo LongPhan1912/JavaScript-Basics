b285e91c1cc942d5e86aff95b0351bc7eb00b5c7
LongPhan1912 <46760280+LongPhan1912@users.noreply.github.com>
2021-01-13 22:55:24 -0500

    remove code relating to global and local scopes

--- functions.js
+++ functions.js
@@ -1,34 +1,3 @@
-/**
-Functions in JavaScript
-*/
-var globalVar = 10;
-
-function fun1() {
-    var scopedVar = 5; // scoped variable; can only be called within fun1()
-    anotherGlobalVar = 2.5;
-}
-function fun2() {
-    var output = "";
-    if (typeof globalVar != "undefined") {
-        output += "my globalVar: " + globalVar;
-    }
-    if (typeof anotherGlobalVar != "undefined") {
-        output += " anotherGlobalVar: " + anotherGlobalVar;
-    }
-    console.log(output);
-}
-fun1();
-fun2();
-
-function fun3() {
-    // local variable takes precendence over global variable
-    var globalVar = 3;
-    return globalVar;
-}
-console.log(fun3()); // will print 3 instead of 10
-console.log(globalVar); // however, this would print 10 as we are calling the global variable
-// good to know, but not a good practice to have 2 variables with the same name
-
 /* function with a return statement. */
 function minusSeven(num) {
     return num - 7;
