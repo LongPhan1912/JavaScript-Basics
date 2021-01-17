0ee01f42d902ee0069a19276aff074e40fdd6527
LongPhan1912 <46760280+LongPhan1912@users.noreply.github.com>
2021-01-16 21:57:31 -0500

    Update exportModule.js

--- Modules/exportModule.js
+++ Modules/exportModule.js
@@ -1,1 +1,11 @@
-exportModule.js
+// first way to export 
+export const capitalise = str => str.toUpperCase();
+
+// or we can do:
+const firstCapitalise = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export { firstCapitalise };
+// export default
+export default function foo() { return 42; }
