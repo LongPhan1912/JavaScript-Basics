d4a742f0404e89d11f0d0079d03d3aa5a327fc27
LongPhan1912 <46760280+LongPhan1912@users.noreply.github.com>
2021-01-16 21:57:07 -0500

    Update importModule.js

--- Modules/importModule.js
+++ Modules/importModule.js
@@ -1,1 +1,8 @@
-modules.js
+// to import a specified variable / function from another module
+import { capitalise } from "../exportModule.js"
+// to import all
+import * as capitaliseStrings from "../exportModule.js"
+// import a default export
+// no need for the curly braces
+import foo from "../exportModule.js"
+console.log(foo());
